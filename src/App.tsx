92a9246062688960629689ac98ac3a4d8752e15a
Prasob kumar kp <prasobkumarkp@gmail.com>
2018-11-04 17:32:29 +0530

    Added callback
    Added callback when ITI has a change:-
     - change in dial code or
    - change in user filed (phone number filed.)

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
 import * as React from 'react';
 import './App.css';
 import Iti from './Components/iti';
-// import logo from './logo.svg';
+import logo from './logo.svg';
 
-
-class App extends React.Component {
+interface IAppState {
+  phoneNumber: string;
+}
+class App extends React.Component<{}, IAppState> {
+  constructor(props: any) {
+    super(props)
+    this.state = { phoneNumber: "" }
+  }
   public render() {
     return (
       <div className="App">
-        {/* <header className="App-header">
+        <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header> */}
-        <Iti />
+          <h1 className="App-title">Explore International Telephone Input in React</h1>
+        </header>
+        <div className="App-intro">
+          <Iti showSearchBox={false} defaultCountryCode={"1"} optionSelectedCallback={phoneNumber => this.setState(() => ({ phoneNumber }))} />
+          <input type="text" className="phone-number" defaultValue={this.state.phoneNumber} placeholder="Display box" readOnly={true} />
+        </div>
       </div>
     );
   }
